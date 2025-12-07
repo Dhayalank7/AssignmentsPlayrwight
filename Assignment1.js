66530ac98634f67f24948601616d9a74a5ecbff8
Dhayalank7 <55044270+Dhayalank7@users.noreply.github.com>
2025-12-07 08:26:10 +0530

    Add files via upload
    Assignment1

---
+++ Assignment1.js
@@ -0,0 +1,10 @@
+let firstName="Dhayalan"
+let companyName="Infosys"
+let mobileNumber=8939271089
+let isAutomation=true
+let hasPlaywright
+console.log(firstName+" "+typeof firstName)
+console.log(companyName+" "+typeof companyName)
+console.log(mobileNumber+" "+typeof mobileNumber)
+console.log(isAutomation+" "+typeof isAutomation)
+console.log(hasPlaywright+" "+typeof hasPlaywright)
